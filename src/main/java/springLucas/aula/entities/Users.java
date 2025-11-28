a6e1a52771d41763b5a643fa1c852daeabe02795
lucashenrique.info@gmail.com <lucashenrique.info@gmail.com>
2025-11-28 16:36:21 -0300

    Order, instant, iso 8601

--- src/main/java/springLucas/aula/entities/Users.java
+++ src/main/java/springLucas/aula/entities/Users.java
@@ -1,11 +1,10 @@
 package springLucas.aula.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -20,6 +19,9 @@ public class Users implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public Users(){
     }
@@ -70,6 +72,10 @@ public class Users implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
