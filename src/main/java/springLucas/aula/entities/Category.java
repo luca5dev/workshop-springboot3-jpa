946025b10554f87e997532d982c13852152c33aa
lucashenrique.info@gmail.com <lucashenrique.info@gmail.com>
2025-12-04 16:36:24 -0300

    Associação muitos para muitos com o JoinTable

--- src/main/java/springLucas/aula/entities/Category.java
+++ src/main/java/springLucas/aula/entities/Category.java
@@ -1,5 +1,6 @@
 package springLucas.aula.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -43,7 +44,8 @@ public class Category implements Serializable {
         this.name = name;
     }
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     public Set<Product> getProducts() {
         return products;
     }
